a92511395dd64b3d61dbb04248ef6f552cb001ae
tanishidatta17-dev <tanishi.datta.17@gmail.com>
2026-09-16 15:49:38 +0000

    Day 1

---
+++ day1.py
@@ -0,0 +1,8 @@
+# Day 1 - Python for Bioinformatics
+print("hello Bioinformaticians!")
+
+dna="ATGCGTACGTAGCTAGCTAG"
+print( f"My DNA: {dna}")
+print(f"Lenght: {len(dna)}")
+print(f"A count: {dna.count('A')}")
+
